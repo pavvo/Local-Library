266ab2c674de1e44becb84933af8559ee0cd061c
Pavle Vojinovic <vv.pavlee@gmail.com>
2018-04-16 20:34:08 +0200

    Update #2

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
@@ -26,3 +27,10 @@ def index(request):
                                                   # 'particular_book': particular_book,
     }
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    # paginate_by = 2
+
+class BookDetailView(generic.DetailView):
+    model = Book
